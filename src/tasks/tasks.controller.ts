73b7d6f5436f1a3e75086905cadb3eb395966d39
Lionel Mukuna Ciowela <liyokuna@yahoo.Fr>
2025-02-25 17:32:17 +0100

    mapped and exception error

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,15 +1,22 @@
 import {
+  BadRequestException,
   Body,
   Controller,
+  Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   NotFoundException,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { ITask } from './task.model';
 import { CreateTaskDto } from './create-task.dto';
 import { FindOneParams } from './find-one.params';
+import { UpdateTaskStatusDto } from './update-task-status.dto';
+import { WrongTaskStatusException } from './exceptions/wrong-task-status.exception';
 
 @Controller('tasks') // prefic for routes
 export class TasksController {
@@ -21,18 +28,56 @@ export class TasksController {
   }
 
   @Get('/:id')
-  public findOne(@Param('id') params: FindOneParams): ITask {
-    const task: ITask | undefined = this.tasksService.findOne(params.id);
-
-    if (task) {
-      return task;
-    }
-
-    throw new NotFoundException();
+  public findOne(@Param() params: FindOneParams): ITask {
+    return this.findOneOrFail(params.id);
   }
 
   @Post()
   public create(@Body() createTaskDto: CreateTaskDto): ITask {
     return this.tasksService.create(createTaskDto);
+  }
+
+  /*@Patch('/:id/status')
+  public updateTaskStatus(
+    @Param() params: FindOneParams,
+    @Body() updateTaskStatus: UpdateTaskStatusDto,
+  ): ITask {
+    const task = this.findOneOrFail(params.id);
+    task.status = updateTaskStatus.status;
+
+    return task;
+  }*/
+
+  @Patch('/:id')
+  public updateTask(
+    @Param() params: FindOneParams,
+    @Body() updateTaskDto: UpdateTaskStatusDto,
+  ): ITask {
+    const task = this.findOneOrFail(params.id);
+    try {
+      return this.tasksService.updateTask(task, updateTaskDto);
+    } catch (error) {
+      if (error instanceof WrongTaskStatusException) {
+        throw new BadRequestException([error.message]);
+      }
+      throw error;
+    }
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public deleteTask(@Param() params: FindOneParams): void {
+    const task = this.findOneOrFail(params.id);
+    this.tasksService.deleteTask(task);
+  }
+
+  private findOneOrFail(id: string): ITask {
+    const task: ITask | undefined = this.tasksService.findOne(id);
+
+    if (!task) {
+      throw new NotFoundException();
+    }
+
+    return task;
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { ITask } from './task.model';
+import { ITask, TaskStatus } from './task.model';
 import { CreateTaskDto } from './create-task.dto';
 import { randomUUID } from 'crypto';
+import { UpdateTaskDto } from './update-task.dto';
+import { WrongTaskStatusException } from './exceptions/wrong-task-status.exception';
 
 @Injectable()
 export class TasksService {
@@ -15,6 +17,17 @@ export class TasksService {
     return this.tasks.find((task) => task.id === id);
   }
 
+  public updateTask(task: ITask, updateTaskDto: UpdateTaskDto): ITask {
+    if (
+      updateTaskDto.status &&
+      !this.isValidStatusTransition(task.status, updateTaskDto.status)
+    ) {
+      throw new WrongTaskStatusException();
+    }
+    Object.assign(task, updateTaskDto);
+    return task;
+  }
+
   create(createTaskDto: CreateTaskDto): ITask {
     const task: ITask = {
       id: randomUUID(),
@@ -22,5 +35,23 @@ export class TasksService {
     };
     this.tasks.push(task);
     return task;
+  }
+
+  deleteTask(task: ITask): void {
+    this.tasks = this.tasks.filter(
+      (filteredTask) => filteredTask.id !== task.id,
+    );
+  }
+
+  private isValidStatusTransition(
+    currentStatus: TaskStatus,
+    newStatus: TaskStatus,
+  ): boolean {
+    const statusOrder = [
+      TaskStatus.OPEN,
+      TaskStatus.IN_PROGRESS,
+      TaskStatus.DONE,
+    ];
+    return statusOrder.indexOf(currentStatus) <= statusOrder.indexOf(newStatus);
   }
 }
