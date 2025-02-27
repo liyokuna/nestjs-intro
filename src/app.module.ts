0d730e152d4bf3253f1012f0b4545870a3d690ae
Lionel Mukuna Ciowela <liyokuna@yahoo.Fr>
2025-02-27 14:22:42 +0100

    joi and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,22 @@ import { DummyService } from './dummy/dummy.service';
 import { LoggerService } from './logger/logger.service';
 import { messageFromatter } from './helpers/message-formatter.helper';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
+import { appConfig } from 'config/app.config';
+import { appConfigSchema } from 'config/config.types';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig],
+      validationSchema: appConfigSchema,
+      validationOptions: {
+        allowUnknown: true, // every single time nest sj run time and found an exception it throw an exception
+        abortEarly: true, //
+      },
+    }),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService, DummyService, LoggerService, messageFromatter], // necessaire car les classe votn être resolus par nest js pour faire de la dépendance d'injection
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { DummyService } from './dummy/dummy.service';
 import { LoggerService } from './logger/logger.service';
+import { ConfigService } from '@nestjs/config';
+import { ConfigType } from 'config/config.types';
+import { AppConfig } from 'config/app.config';
 
 @Injectable() // necessaire car il epermt de dire à nestjs de resoudre les dépendances
 export class AppService {
   constructor(
     private readonly dummyService: DummyService,
     private readonly loggerService: LoggerService,
+    private readonly configService: ConfigService<ConfigType>,
   ) {}
   getHello(): string {
-    return `Hello World! ${this.loggerService.log()}`;
+    const prefix = this.configService.get<AppConfig>('app')?.messagePrefix;
+    return `${prefix} Hello World! ${this.loggerService.log()}`;
   }
 }
